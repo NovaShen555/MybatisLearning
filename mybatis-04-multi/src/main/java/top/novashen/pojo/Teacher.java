49d32c6f1a1d163cd24fe171808821be1451e1b3
NovaShen555 <admin@novashen.top>
2023-03-19 18:49:15 +0800

    实现多对一

--- mybatis-04-multi/src/main/java/top/novashen/pojo/Teacher.java
+++ mybatis-04-multi/src/main/java/top/novashen/pojo/Teacher.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Teacher {
     private int id;
     private String name;
+
+    private List<Student> studentList;
 }
